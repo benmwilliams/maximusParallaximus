d7f4b76b5398bce6e04fc8ee5657be6593fb6693
Ben Williams <williams.ben0085@gmail.com>
2015-09-23 21:29:58 -0400

    removes console.log

--- parallax.js
+++ parallax.js
@@ -2,16 +2,16 @@ var $image = $('.image'),
     $layer = $('div[class*="layer-"]'),
     w = $(window).width(),
     h = $(window).height();
-    console.log($image);
+
 $(window).on('mousemove', function(e) {
     var offsetX = 0.5 - e.pageX / w,
         offsetY = 0.5 - e.pageY / h,
         offsetImage = $image.data('offset'),
         transformImage = 'translateY(' + (-offsetX * offsetImage) + 'px) rotateX(' + (-offsetY * offsetImage) + 'deg) rotateY(' + (offsetX * (offsetImage * 2)) + 'deg)';
-    console.log($('.image'));
+
     $('.image').css('transform', transformImage);
 
-    $('div[class*="layer-"]').each(function() {
+    $layer.each(function() {
         var $this = $(this),
             offsetLayer = $(this).data("offset") || 0,
             transformLayer = 'translateX(' + offsetX * offsetLayer +'px) translateY(' + offsetY * offsetLayer + 'px)';
